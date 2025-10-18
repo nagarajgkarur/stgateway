b71d84902fd831543a7682b25ecdd305d32030eb
Nagaraj <nagaraj.g.karur@gmail.com>
2025-10-18 16:56:47 +0530

    initial commit for stgateway service

---
+++ src/main/java/com/stock/management/stgateway/StgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.stock.management.stgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StgatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stock/management/stgateway/StgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock.management.stgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
